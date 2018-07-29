84490212c07a76e22e30e375ac8cbfeb4c76aee4
Ain Sal <ain@abcd.sa>
2018-07-29 07:16:47 +0300

    Update a couple of outdated examples
    Closes #10

--- examples/basic.js
+++ examples/basic.js
@@ -1,33 +1,30 @@
 import React from 'react';
+
 import { Button, View } from 'react-native';
 
-import Behavior from '../behavior';
+import Behavior from '../src/behavior';
 
-const Example = () => {
-  let goTo;
+const Basic = () => {
+  const basic = React.createRef();
+
+  const goTo = state => basic.current.goTo(state);
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
+    <View style={styles.container}>
+      <View style={styles.behaviorContainer}>
         <Behavior
-          ref={ref => (goTo = ref.goTo)}
-          states={[
+          ref={basic}
+          state={[
             { backgroundColor: '#d8d8d8', scale: 1 },
             { backgroundColor: '#4285f4', scale: 2.5 },
             { backgroundColor: '#0f9d58', scale: 5 },
             { backgroundColor: '#f4b400', scale: 10 }
           ]}
-          style={{
-            borderRadius: 10,
-            height: 20,
-            overflow: 'hidden',
-            width: 20
-          }}
-          indices={[0, 1, 2, 3]} // android only
+          style={styles.behavior}
         />
       </View>
 
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+      <View style={styles.steps}>
         <Button color="#d8d8d8" title="1x" onPress={() => goTo(0)} />
         <Button color="#4285f4" title="2.5x" onPress={() => goTo(1)} />
         <Button color="#0f9d58" title="5x" onPress={() => goTo(2)} />
@@ -37,4 +34,20 @@ const Example = () => {
   );
 };
 
-export default Example;
+const styles = {
+  container: { flex: 1 },
+  behaviorContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  behavior: {
+    borderRadius: 10,
+    height: 20,
+    overflow: 'hidden',
+    width: 20
+  },
+  steps: { flexDirection: 'row', justifyContent: 'space-around' }
+};
+
+export default Basic;
--- examples/box.js
+++ examples/box.js
@@ -1,18 +1,24 @@
 import React from 'react';
+
 import { Button, View } from 'react-native';
 
-import Behavior from '../behavior';
+import Behavior from '../src/behavior';
 
-const Example = () => {
-  let goTo;
+const Box = () => {
+  const box = React.createRef();
+
+  const goTo = state => box.current.goTo(state);
+
+  const play = () => goTo([1, 2, 3, 4, 5, 6, 7, 8]);
+  const reset = () => goTo([7, 6, 5, 4, 3, 2, 1, 0]);
 
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ alignItems: 'center', flex: 1, justifyContent: 'center' }}>
+    <View style={styles.container}>
+      <View style={styles.behaviorContainer}>
         <Behavior
-          config={{ mode: 'timing', duration: 250 }}
-          ref={ref => (goTo = ref.goTo)}
-          states={[
+          config={{ type: 'timing', duration: 250 }}
+          ref={box}
+          state={[
             { backgroundColor: '#d8d8d8', height: 100, width: 100 },
             { backgroundColor: '#0f9d58' },
             { height: 150 },
@@ -23,16 +29,10 @@ const Example = () => {
             { translateY: -50 },
             { width: 150 }
           ]}
-          indices={[0, 1, 2, 3, 4, 5, 6, 7, 8]} // android only
         />
       </View>
 
-      <View
-        style={{
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          justifyContent: 'space-between'
-        }}>
+      <View style={styles.steps}>
         <Button color="#d8d8d8" title="original" onPress={() => goTo(0)} />
         <Button color="#d8d8d8" title="backgroundC" onPress={() => goTo(1)} />
         <Button color="#d8d8d8" title="height" onPress={() => goTo(2)} />
@@ -44,25 +44,37 @@ const Example = () => {
         <Button color="#d8d8d8" title="width" onPress={() => goTo(8)} />
       </View>
 
-      <View
-        style={{
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          justifyContent: 'center'
-        }}>
-        <Button
-          color="#d8d8d8"
-          title="play"
-          onPress={() => goTo([1, 2, 3, 4, 5, 6, 7, 8])}
-        />
-        <Button
-          color="#d8d8d8"
-          title="reset"
-          onPress={() => goTo([7, 6, 5, 4, 3, 2, 1, 0])}
-        />
+      <View style={styles.actions}>
+        <Button color="#d8d8d8" title="play" onPress={play} />
+        <Button color="#d8d8d8" title="reset" onPress={reset} />
       </View>
     </View>
   );
 };
 
-export default Example;
+const styles = {
+  container: { flex: 1 },
+  behaviorContainer: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  behavior: {
+    borderRadius: 10,
+    height: 20,
+    overflow: 'hidden',
+    width: 20
+  },
+  steps: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  actions: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center'
+  }
+};
+
+export default Box;
