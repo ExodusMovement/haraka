01a694ae99823134366beb1076d01b9314b239be
Rachmad Nafisholeh <rachmadnafisholeh@gmail.com>
2022-04-07 23:33:39 +0700

    style: add safe checking when accessing animation state

--- src/behavior.js
+++ src/behavior.js
@@ -256,7 +256,9 @@ export default class Behavior extends React.PureComponent {
     }
 
     const addProp = (prop, defaultValue) => {
-      if (freeze) return state[this.key][prop] ?? defaultValue
+      if (freeze) {
+        return (state[this.key] && state[this.key][prop]) || defaultValue
+      }
       return this.nativeDriver.interpolate({
         inputRange,
         outputRange: getRange(prop, defaultValue),
